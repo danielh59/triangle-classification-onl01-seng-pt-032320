35c694ba0bddf2922a20258678be079a3410dfb2
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 18:58:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,9 @@ def initialize(side1, side2, side3)
 end
 
 def kind
-   if (@side1 <= 0) || (@side1 <= 0) || (@side1 <= 0)
+   if @side1 <= 0 || @side1 <= 0 || @side1 <= 0
      raise TriangleError
-   elsif (@side1 + @side1 <= @side1) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
+   elsif @side1 + @side1 <= @side1 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
      raise TriangleError
    else
      if (@side1 == @side2) && (@side2 == @side3)
