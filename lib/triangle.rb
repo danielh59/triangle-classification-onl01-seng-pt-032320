e2f971d90d147f438380d250355fe969e63e5abf
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 18:58:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ def kind
    elsif @side1 + @side1 <= @side1 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
      raise TriangleError
    else
-     if (@side1 == @side2) && (@side2 == @side3)
+     if @side1 == @side2 && @side2 == @side3
        :equilateral
      elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
        :scalene
