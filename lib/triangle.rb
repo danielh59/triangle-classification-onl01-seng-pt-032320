446aa00aa0305b7dfa9889312e370c8be0a2793e
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 19:10:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ end
 def kind
    if @side1 <= 0 || @side1 <= 0 || @side1 <= 0
      raise TriangleError
-   elsif @side1 + @side1 <= @side1 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
+   elsif @side1 + @side1 <= @side1 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2 
      raise TriangleError
    else
      if @side1 == @side2 && @side2 == @side3
@@ -25,3 +25,4 @@ def kind
    class TriangleError < StandardError
    end
  end
+
