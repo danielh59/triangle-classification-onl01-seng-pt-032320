14598d4654da1348bcbb8bd669fa9316efa2397f
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 19:11:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ def kind
    if @side1 <= 0 || @side1 <= 0 || @side1 <= 0
      raise TriangleError
    elsif @side1 + @side1 <= @side1 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
-    #  raise TriangleError
+     raise TriangleError
    else
      if @side1 == @side2 && @side2 == @side3
        :equilateral
