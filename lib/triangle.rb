60937bfaf085749bea4f6e72c58f0d1bf9c90ba7
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 18:59:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ def kind
        :equilateral
      elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
        :scalene
-     elsif @side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+     elsif @side1 == @side2 || @side2 == @side3) || (@side1 == @side3)
        :isosceles
      end
      end
