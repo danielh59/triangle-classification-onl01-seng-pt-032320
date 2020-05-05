cfbafff4ce57f9ba6e0c86a81b6e3da9b1c26026
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 18:48:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,10 +8,19 @@ def initialize(side1, side2, side3)
 end
 
 def kind
-case self
-when @side1 == @side2 && @side2 == @side3
-  :equilateral
-end
-end
+   if (@side1 <= 0) || (@side1 <= 0) || (@side1 <= 0)
+     raise TriangleError
+   elsif (@side1 + @side1 <= @side1) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
+     raise TriangleError
+   else
+     if (@side_1 == @side_2) && (@side_2 == @side_3)
+       :equilateral
+     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+       :isosceles
+     elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+       :scalene
+     end
+   end
 
+ end
 end
